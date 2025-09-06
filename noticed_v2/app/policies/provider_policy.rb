930371de5f4375e92118950d759820f15c13d329
Felipe Morais <felipehhenriquee@gmail.com>
2025-09-06 02:23:08 -0300

    Add Pundit authorization and policies

---
+++ noticed_v2/app/policies/provider_policy.rb
@@ -0,0 +1,27 @@
+class ProviderPolicy < ApplicationPolicy
+  def index?
+    admin?
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    admin?
+  end
+
+  def update?
+    admin?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      admin? ? scope.all : scope.none
+    end
+  end
+end
